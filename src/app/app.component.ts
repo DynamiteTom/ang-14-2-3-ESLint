1f72ead236745260b63b8f677cdd61e16b15c9d4
Gordon T Cumming <dynaphoneimages@hotmail.com>
2022-09-20 19:51:18 +0100

    Standalone 14.2.3 with NgIf instead of CommonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [NgIf],
+  template: `<h1>Hi Angular 14.2.3</h1>
+  
+  <button (click)="toggle()">toggle<button>
+  <br/>
+  <p *ngIf="show">Toggled</p>
+  `,
 })
 export class AppComponent {
   title = 'ang-14-2-3-ESLint';
+  show = false;
+
+  toggle(){
+    this.show = !this.show;
+  }
 }
